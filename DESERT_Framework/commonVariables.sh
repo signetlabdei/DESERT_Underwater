ebaecdf5abb5ce95d8b47ff17166732d5d582749
Federico Favaro <fedefava86@gmail.com>
2017-07-17 22:34:07 +0200

    Release desert-3.0.2
    * Add support and installer script to EvoLogics sandbox
    * Imported a new patch for NS for real-time scheduler improvements
    * Upload the right version of WOSS (1.5.1)
    
    Fix #1

--- DESERT_Framework/commonVariables.sh
+++ DESERT_Framework/commonVariables.sh
@@ -11,20 +11,20 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University of Padova (SIGNET lab) nor the 
-#    names of its contributors may be used to endorse or promote products 
+# 3. Neither the name of the University of Padova (SIGNET lab) nor the
+#    names of its contributors may be used to endorse or promote products
 #    derived from this software without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # @name_file:   commonVariable.sh
@@ -39,7 +39,7 @@
 # Versions
 
 #***
-# _DEBUG: 0(no messages printed) 
+# _DEBUG: 0(no messages printed)
 #         1(messages printed)
 export _DEBUG=0
 #*
@@ -98,7 +98,7 @@ export WOSS_VERSION=1.5.1
 export WOSS_TAR_FILE=${ROOT_DESERT}/woss-${WOSS_VERSION}.tar.gz
 export WOSS_DIR=woss-${WOSS_VERSION}
 
-export DESERT_VERSION=3.0.0
+export DESERT_VERSION=3.0.2
 export DESERT_DIR=DESERT
 
 export UNPACKED_FOLDER="${ROOT_DESERT}/.unpacked_folder"
@@ -107,7 +107,7 @@ export ZXX="${ROOT_DESERT}/.zxx"
 
 #***
 # _WIZARD_OPT: -1(no variable setted)
-#               0(wizard mode is tour-off) 
+#               0(wizard mode is tour-off)
 #               1(wizard mode is tour-on)
 export _WIZARD_OPT=-1
 #*
@@ -155,7 +155,7 @@ export _ADDONS=-1
 #*
 
 #***
-# WITHWOSS:   0(--without-woss options is passed to installer procedure) 
+# WITHWOSS:   0(--without-woss options is passed to installer procedure)
 #             1(--with-woss options in passed to installer procedure)
 export WITHWOSS=0
 #*
@@ -187,4 +187,3 @@ export ADDONS_FILE=".addon.list"
 export TMP_INPUT=""
 export PARAMETER4INSTALLER=""
 # #
-
