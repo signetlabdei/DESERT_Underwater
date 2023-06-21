53c801cbd2a276c5db85f39cf5f696110c7dee65
ciminoV <ciminovincenzo1@gmail.com>
2023-06-21 12:06:37 +0200

    uwswarm_control: add cross layer message class
    This class allows the tracker module to send tracking position data to mission coordinator

---
+++ DESERT_Addons/uwswarm_control/uwsc-clmsg.cc
@@ -0,0 +1,68 @@
+//
+// Copyright (c) 2017 Regents of the SIGNET lab, University of Padova.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Padova (SIGNET lab) nor the
+// names of its contributors may be used to endorse or promote products
+// derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/**
+* @file uswc-clmsg.cc
+* @author Vincenzo Cimino
+* @version 1.0.0
+*
+* \brief Provides the <i>UWSC-CLMSG</i> class implementation.
+*
+* Provides the <i>UWSC-CLMSG</i> class implementation.
+*/
+
+#include "uwsc-clmsg.h"
+
+ClMsgTrack2McPosition::ClMsgTrack2McPosition(int dest_id)
+	: ClMessage(CLMSG_TRACK2MC_VERBOSITY,
+		CLMSG_TRACK2MC_TRACKPOS, UNICAST, dest_id)
+	, tracking_position(nullptr)
+{
+}
+
+ClMsgTrack2McPosition::~ClMsgTrack2McPosition()
+{
+}
+
+ClMsgTrack2McPosition* 
+ClMsgTrack2McPosition::copy()
+{
+  return new ClMsgTrack2McPosition(*this);
+}
+
+void
+ClMsgTrack2McPosition::setTrackPosition(UWSMPosition* position)
+{
+	tracking_position = position;
+}
+
+UWSMPosition*
+ClMsgTrack2McPosition::getTrackPosition() const
+{
+	return tracking_position;
+}
---
+++ DESERT_Addons/uwswarm_control/uwsc-clmsg.h
@@ -0,0 +1,95 @@
+//
+// Copyright (c) 2017 Regents of the SIGNET lab, University of Padova.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Padova (SIGNET lab) nor the
+// names of its contributors may be used to endorse or promote products
+// derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/**
+* @file uswc-clmsg.h
+* @author Vincenzo Cimino
+* @version 1.0.0
+*
+* \brief Provides the <i>UWSC-CLMSG</i> class implementation.
+*
+* Provides the <i>UWSC-CLMSG</i> class implementation.
+*/
+
+
+#ifndef UWSC_CLMSG_H
+#define UWSC_CLMSG_H
+
+#include <packet.h>
+#include <clmessage.h>
+#include <uwsmposition.h>
+
+#define CLMSG_MC2CTR_VERBOSITY 3
+#define CLMSG_CTR2MC_VERBOSITY 3
+#define CLMSG_TRACK2MC_VERBOSITY 3
+
+extern ClMessage_t CLMSG_MC2CTR_SETPOS;
+extern ClMessage_t CLMSG_CTR2MC_GETPOS;
+extern ClMessage_t CLMSG_TRACK2MC_TRACKPOS;
+
+class ClSAP;
+
+/**
+ * Message to get the tracking position from uwtracker addon
+ */
+class ClMsgTrack2McPosition : public ClMessage
+{
+  public:
+
+	/**
+	 * Class constructor
+     * @param dest_id: id of the destination module
+	 */
+    ClMsgTrack2McPosition(int dest_id);
+        
+    virtual ~ClMsgTrack2McPosition();
+
+    /**
+     * Creates a copy of the object
+     * @return Pointer to a copy of the object
+     */
+    ClMsgTrack2McPosition* copy();
+
+    /**
+     * Sets the tracking position received from uwtracker
+     * @param track_position Tracked position from uwtracker
+     */
+    void setTrackPosition(UWSMPosition* track_position);
+
+    /**
+     * Get the tracking position received from uwtracker
+     * @return track_position Tracked position from uwtracker
+     */
+    UWSMPosition* getTrackPosition() const;
+    
+    
+  private:
+    UWSMPosition* tracking_position; /*< Tracking position sent by the uwtracker addon */
+};
+
+#endif /* UWSC_CLMSG_H */
