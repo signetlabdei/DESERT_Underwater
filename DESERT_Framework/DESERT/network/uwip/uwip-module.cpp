d19469d6bde8f34a6f7f6cfdfa816fe1d42aa985
ciminoV <ciminovincenzo1@gmail.com>
2025-04-18 14:28:31 +0200

    update uwip: refactor and add log

--- DESERT_Framework/DESERT/network/uwip/uwip-module.cpp
+++ DESERT_Framework/DESERT/network/uwip/uwip-module.cpp
@@ -38,6 +38,8 @@
 
 #include "uwip-module.h"
 #include "uwip-clmsg.h"
+#include <packet.h>
+#include <sstream>
 
 extern packet_t PT_UWIP;
 
@@ -86,10 +88,6 @@ UWIPModule::UWIPModule()
 	ipAddr_ = ++lastIP;
 }
 
-UWIPModule::~UWIPModule()
-{
-}
-
 int
 UWIPModule::command(int argc, const char *const *argv)
 {
@@ -105,11 +103,7 @@ UWIPModule::command(int argc, const char *const *argv)
 			addr_type_inet = false;
 			return TCL_OK;
 		} else if (strcasecmp(argv[1], "addr-string") == 0) {
-			tcl.resultf("%d.%d.%d.%d",
-					(ipAddr_ & 0xff000000) >> 24,
-					(ipAddr_ & 0x00ff0000) >> 16,
-					(ipAddr_ & 0x0000ff00) >> 8,
-					(ipAddr_ & 0x000000ff));
+			tcl.resultf("%s", printIP(ipAddr_).c_str());
 			return TCL_OK;
 		} else if (strcasecmp(argv[1], "getipheadersize") == 0) {
 			tcl.resultf("%d", this->getIpHeaderSize());
@@ -120,11 +114,13 @@ UWIPModule::command(int argc, const char *const *argv)
 		}
 	} else if (argc == 3) {
 		if (strcasecmp(argv[1], "addr") == 0) {
-			ipAddr_ = static_cast<uint8_t>(atoi(argv[2]));
-			if (ipAddr_ == 0) {
-				fprintf(stderr, "0 is not a valid IP address");
+			int addr = std::atoi(argv[2]);
+			if (addr <= 0 || addr > 255) {
+				tcl.resultf("invalid IP address: %d", addr);
 				return TCL_ERROR;
 			}
+
+			ipAddr_ = static_cast<uint8_t>(addr);
 			return TCL_OK;
 		}
 	}
@@ -145,8 +141,7 @@ UWIPModule::recv(Packet *p)
 				(ch->next_hop() & 0x000000ff) ==
 						(UWIP_BROADCAST & 0x000000ff) ||
 				(iph->daddr() & 0x000000ff) == (ipAddr_ & 0x000000ff) ||
-				(iph->daddr() & 0x000000ff) ==
-						(UWIP_BROADCAST & 0x000000ff)) { // Right destination.
+				(iph->daddr() & 0x000000ff) == (UWIP_BROADCAST & 0x000000ff)) {
 			ch->size() -= sizeof(hdr_uwip);
 			sendUp(p);
 			return;
@@ -154,28 +149,20 @@ UWIPModule::recv(Packet *p)
 			drop(p, 1, NOT_FOR_ME_REASON);
 			return;
 		}
-	} else if (ch->direction() == hdr_cmn::DOWN) { // Direction DOWN
-		if (iph->daddr() == ipAddr_ ||
-				ch->next_hop() == ipAddr_) { // The node is sending a packet to
-											 // itself, drop it.
+	} else if (ch->direction() == hdr_cmn::DOWN) {
+		if (iph->daddr() == ipAddr_ || ch->next_hop() == ipAddr_) {
 			drop(p, 1, INVALID_DESTINATION_ADDR);
 			return;
 		}
-		if (addr_type_inet) {
-			ch->addr_type() = NS_AF_INET;
-		} else {
-			ch->addr_type() = NS_AF_ILINK;
-		}
 
+		ch->addr_type() = addr_type_inet ? NS_AF_INET : NS_AF_ILINK;
 		ch->size() += sizeof(hdr_uwip);
 
-		if (iph->saddr() == 0) {
+		if (iph->saddr() == 0)
 			iph->saddr() = ipAddr_;
-		}
 
-		if (ch->prev_hop_ == 0) {
+		if (ch->prev_hop_ == 0)
 			ch->prev_hop_ = ipAddr_;
-		}
 
 		if (iph->daddr() == 0) {
 			drop(p, 1, DESTINATION_ADDR_UNSET);
@@ -183,10 +170,10 @@ UWIPModule::recv(Packet *p)
 		}
 
 		if (ch->next_hop() == 0 && iph->daddr() != 0) {
-			if (debug_)
-				cerr << "Node:" << this->printIP(ipAddr_)
-					 << ":Warning:Packet sent with next_hop equals to 0"
-					 << endl;
+			printOnLog(Logger::LogLevel::ERROR,
+					"UWIP",
+					"recv(Packet *)::packet sent with next_hop equals to 0");
+
 			ch->next_hop() = iph->daddr();
 		}
 
@@ -214,7 +201,7 @@ UWIPModule::recvSyncClMsg(ClMessage *m)
 const string
 UWIPModule::printIP(const nsaddr_t &ip_)
 {
-	stringstream out;
+	std::stringstream out;
 	out << ((ip_ & 0xff000000) >> 24);
 	out << ".";
 	out << ((ip_ & 0x00ff0000) >> 16);
